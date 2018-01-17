b73932a7d2e659ada15d89b260150cb67cfebe39
Aleksey Kuzmenko (550502) <kuzmenko_aleshka@bk.ru>
2018-01-17 05:47:22 +0300

    Удаление неиспользуемого класса

--- JavaGame/src/Classes/Records.java
+++
@@ -1,9 +0,0 @@
-public class Records {
-    java.lang.String Name;
-    Double Time;
-
-    public Records(java.lang.String Name, double Time) {
-        this.Name = Name;
-        this.Time = Time;
-    }
-}
